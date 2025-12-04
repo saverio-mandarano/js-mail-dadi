132c2861ae1e4c54802e26c0d3c457ba2b0460bd
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-04 16:49:05 +0100

    Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.

--- js/dadi.js
+++ js/dadi.js
@@ -1,14 +1,9 @@
 // Gioco dei dadi
 // Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+const userDiceRoll = Math.floor(Math.random() * 6) + 1;
+const computerDiceRoll = Math.floor(Math.random() * 6) + 1;
+console.log(`User dice roll: ${userDiceRoll}`);
+console.log(`Computer dice roll: ${computerDiceRoll}`);
+
+
 // Stabilire il vincitore, in base a chi fa il punteggio più alto.
-
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
-// Consigli del giorno:
-// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// si ma noi cosa vogliamo fare?
-// torniamo a scrivere in italiano
-// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
