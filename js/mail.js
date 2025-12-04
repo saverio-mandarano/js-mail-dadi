5cd5b27b078a06bf8f71accbaefbb96d1943cd38
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-04 16:10:45 +0100

    // Controlla che sia nella lista di chi può accedere.

--- js/mail.js
+++ js/mail.js
@@ -14,27 +14,26 @@ const mailingList = [
 ];
 console.log(mailingList);
 
-// Chiedi all’utente la sua email,
-const userEmail = prompt(`Inserisci il tuo indirizzo email:`);
+
+// Chiedi all’utente la sua email.
+const userEmail = prompt(`Inserisci il tuo indirizzo email completo:`);
+console.log(`l'email dell'utente è: ${userEmail}`);
 
 
-// controlla che sia nella lista di chi può accedere,
+// Controlla che sia nella lista di chi può accedere.
+let invited = false;
+for (let i=0; i<mailingList.length && invited === false; i++){
+
+    if (userEmail === mailingList[i]){
+        invited = true;
+    }
+
+}
+
+
 // stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
 // Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
-// Si può fare? Certo che si basta ragionare un po’.
+
 
 // Nota: 
 // Non è necessario provvedere alla validazione delle email
-
-
-// Prima di partire a scrivere codice poniamoci qualche domanda:
-// Che ci sia un array da qualche parte?
-// Se dobbiamo confrontare qualcosa che "cosa" ci serve?
-
-// Consigli del giorno:
-// scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
-// javascript non fa nulla da solo, dobbiamo dirgli noi cosa vogliamo fare
-// si ma noi cosa vogliamo fare?
-// torniamo a scrivere in italiano
-// proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
-
